ac4ba5129738545e4ddd7a2c4fc2461dfcb88e2b
Kristian <kristian@ludensreklame.no>
2017-03-06 09:51:20 +0100

    Replaced the switches with math

--- src/Column.js
+++ src/Column.js
@@ -10,275 +10,38 @@ import { css } from 'glamor'
 
 export default class Column extends Component {
   largify(gridSize) {
+    const gridWidth = 100 / gridSize
+    const styles = css({
+      width: gridWidth+'%',
+      float: 'left'
+    })
 
-    const styles = { width: '100%' }
-
-    switch(gridSize) {
-      case "1" :
-        return css({
-          ...styles,
-          width: '100%',
-          float: 'left'
-        })
-      case "2" :
-        return css({
-          ...styles,
-          width: '50%',
-          float: 'left'
-        })
-      case "3" :
-        return css({
-          ...styles,
-          width: '33.33332%',
-          float: 'left'
-        })
-      case "4" :
-        return css({
-          ...styles,
-          width: '25%',
-          float: 'left'
-        })
-      case "5" :
-        return css({
-          ...styles,
-          width: '20%',
-          float: 'left'
-        })
-      case "6" :
-        return css({
-          ...styles,
-          width: '16.666666666667%',
-          float: 'left'
-        })
-      case "7" :
-        return css({
-          ...styles,
-          width: '14.28571428%',
-          float: 'left'
-        })
-      case "8" :
-        return css({
-          ...styles,
-          width: '12.5%',
-          float: 'left'
-        })
-      case "9" :
-        return css({
-          ...styles,
-          width: '11.111111111%',
-          float: 'left'
-        })
-      case "10" :
-        return css({
-          ...styles,
-          width: '10%',
-          float: 'left'
-        })
-      case "11" :
-        return css({
-          ...styles,
-          width: '9.0909090909%',
-          float: 'left'
-        })
-      case "12" :
-        return css({
-          ...styles,
-          width: '8.33333333%',
-          float: 'left'
-        })
-      default :
-        return css(styles)
-    }
+    return styles
 
   }
 
   mediumify(gridSize) {
-    switch(gridSize) {
-      case "1" :
-        return css({
-          '@media(max-width: 900px)': {
-            width: '100%'
-          }
-        })
-      case "2" :
-        return css({
-          '@media(max-width: 900px)': {
-            width: '50%',
-            float: 'left',
-          }
-        })
-      case "3" :
-        return css({
-          '@media(max-width: 900px)': {
-            width: '33.33332%',
-            float: 'left',
-          }
-        })
-      case "4" :
-        return css({
-          '@media(max-width: 900px)': {
-            width: '25%',
-            float: 'left',
-          }
-        })
-      case "5" :
-        return css({
-          '@media(max-width: 900px)': {
-            width: '20%',
-            float: 'left',
-          }
-        })
-      case "6" :
-        return css({
-          '@media(max-width: 900px)': {
-            width: '16.666666666667%',
-            float: 'left',
-          }
-        })
-      case "7" :
-        return css({
-          '@media(max-width: 900px)': {
-            width: '14.28571428%',
-            float: 'left',
-          }
-        })
-      case "8" :
-        return css({
-          '@media(max-width: 900px)': {
-            width: '12.5%',
-            float: 'left',
-          }
-        })
-      case "9" :
-        return css({
-          '@media(max-width: 900px)': {
-            width: '11.111111111%',
-            float: 'left',
-          }
-        })
-      case "10" :
-        return css({
-          '@media(max-width: 900px)': {
-            width: '10%',
-            float: 'left',
-          }
-        })
-      case "11" :
-        return css({
-          '@media(max-width: 900px)': {
-            width: '9.0909090909%',
-            float: 'left',
-          }
-        })
-      case "12" :
-        return css({
-          '@media(max-width: 900px)': {
-            width: '8.33333333%',
-            float: 'left',
-          }
-        })
-      default :
-        return css({width: '100%'})
-    }
+    const gridWidth = 100 / gridSize
+    const styles = css({
+      '@media(max-width: 900px)': {
+        width: gridWidth+'%',
+        float: 'left'
+      }
+    })
+
+    return styles
   }
 
   smallify(gridSize) {
-    /*
-    let currentType = ''
-    if (type === 'medium') {
-      currentType = '@media(max-width: 900px)'
-    } else if (type === 'small') {
-      currentType = '@media(max-width: 600px)'
-    }
-    */
+    const gridWidth = 100 / gridSize
+    const styles = css({
+      '@media(max-width: 600px)': {
+        width: gridWidth+'%',
+        float: 'left'
+      }
+    })
 
-    switch(gridSize) {
-      case "1" :
-        return css({
-          '@media(max-width: 600px)': {
-            width: '100%'
-          }
-        })
-      case "2" :
-        return css({
-          '@media(max-width: 600px)': {
-            width: '50%',
-            float: 'left',
-          }
-        })
-      case "3" :
-        return css({
-          '@media(max-width: 600px)': {
-            width: '33.33332%',
-            float: 'left',
-          }
-        })
-      case "4" :
-        return css({
-          '@media(max-width: 600px)': {
-            width: '25%',
-            float: 'left',
-          }
-        })
-      case "5" :
-        return css({
-          '@media(max-width: 600px)': {
-            width: '20%',
-            float: 'left',
-          }
-        })
-      case "6" :
-        return css({
-          '@media(max-width: 600px)': {
-            width: '16.666666666667%',
-            float: 'left',
-          }
-        })
-      case "7" :
-        return css({
-          '@media(max-width: 600px)': {
-            width: '14.28571428%',
-            float: 'left',
-          }
-        })
-      case "8" :
-        return css({
-          '@media(max-width: 600px)': {
-            width: '12.5%',
-            float: 'left',
-          }
-        })
-      case "9" :
-        return css({
-          '@media(max-width: 600px)': {
-            width: '11.111111111%',
-            float: 'left',
-          }
-        })
-      case "10" :
-        return css({
-          '@media(max-width: 600px)': {
-            width: '10%',
-            float: 'left',
-          }
-        })
-      case "11" :
-        return css({
-          '@media(max-width: 600px)': {
-            width: '9.0909090909%',
-            float: 'left',
-          }
-        })
-      case "12" :
-        return css({
-          '@media(max-width: 600px)': {
-            width: '8.33333333%',
-            float: 'left',
-          }
-        })
-      default :
-        return css({width: '100%'})
-    }
+    return styles
   }
 
   paddify(padding) {
